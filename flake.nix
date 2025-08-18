50f550a69ea1cbcf5447467ec47ec500fd0913fc
Alex Kumpula <alex.kumpula01@gmail.com>
2025-08-18 04:53:34 -0600

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Your new nix config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+
+    # Supported systems for your flake packages, shell, etc.
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+  in {
+    overlays = import ./overlays {inherit inputs;};
+    nixosModules = import ./modules/nixos;
+
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      # FIXME - DONE replace with your hostname
+      mainHost = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main nixos configuration file <
+          ./hosts/configuration.nix
+        ];
+      };
+    };
+  };
+}
---
+++ modules/default.nix
@@ -0,0 +1,13 @@
+# Add your reusable NixOS modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
+# These should be stuff you would like to share with others, not your personal configurations.
+{
+  # List your module files here
+  # my-module = import ./my-module.nix;
+
+  gnome = import ./gnome.nix;
+  niri = import ./niri.nix;
+  alacritty = import ./alacritty.nix;
+  git = import ./git.nix;
+  gdm = import ./gdm.nix;
+  xwayland-satellite = import ./xwayland-satellite.nix;
+}

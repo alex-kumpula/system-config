6a6a2c0ea5744d9ed6e17b8ae6a99c0a62bb4a6a
Alex Kumpula <alex.kumpula01@gmail.com>
2025-08-23 01:11:14 -0600

    update nix-ld

--- modules/nix-ld.nix
+++ modules/nix-ld.nix
@@ -3,9 +3,58 @@
   programs.nix-ld = {
     enable = true;
     libraries = with pkgs; [
+      alsa-lib
+      at-spi2-atk
+      at-spi2-core
+      atk
+      cairo
+      cups
+      curl
+      dbus
+      expat
+      fontconfig
+      freetype
+      fuse3
+      gdk-pixbuf
+      glib
+      gtk3
+      icu
+      libGL
+      libappindicator-gtk3
+      libdrm
+      libglvnd
+      libnotify
+      libpulseaudio
+      libunwind
+      libusb1
+      libuuid
+      libxkbcommon
+      libxml2
+      mesa
+      nspr
+      nss
+      openssl
+      pango
+      pipewire
+      stdenv.cc.cc
+      systemd
+      vulkan-loader
+      xorg.libX11
+      xorg.libXScrnSaver
+      xorg.libXcomposite
+      xorg.libXcursor
+      xorg.libXdamage
+      xorg.libXext
+      xorg.libXfixes
+      xorg.libXi
+      xorg.libXrandr
+      xorg.libXrender
+      xorg.libXtst
+      xorg.libxcb
+      xorg.libxkbfile
+      xorg.libxshmfence
       zlib
-      stdenv.cc.cc.lib
-      # add other libs required by your foreign binaries
-    ];
+    ]
+    ++ pkgs.steam-run.args.multiPkgs pkgs; #add all the libraries included in the steam fhs environment
   };
 }

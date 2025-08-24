da4729d5492928ab4eb92db9f446711583eb4928
Alex Kumpula <alex.kumpula01@gmail.com>
2025-08-24 02:06:12 -0600

    add vulkan

--- hosts/alex-laptop/configuration.nix
+++ hosts/alex-laptop/configuration.nix
@@ -107,6 +107,11 @@
   # Enable OpenGL
   hardware.graphics = {
     enable = true;
+    extraPackages = with pkgs; [
+      vulkan-loader
+      vulkan-validation-layers
+      vulkan-extension-layer
+    ];
   };
 
   # Load nvidia driver for Xorg and Wayland

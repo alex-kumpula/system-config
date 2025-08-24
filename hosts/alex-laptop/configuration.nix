234575a450bf52e605a57e56c013cb613821661e
Alex Kumpula <alex.kumpula01@gmail.com>
2025-08-23 20:35:09 -0600

    test

--- hosts/alex-laptop/configuration.nix
+++ hosts/alex-laptop/configuration.nix
@@ -153,7 +153,11 @@
   # VULKAN
   hardware.opengl.driSupport32Bit = true; # Needed for Steam to work, among other things
   hardware.opengl.enable = true;
-  hardware.opengl.extraPackages = with pkgs; [ vulkan-loader lib32.vulkan-loader ];
+  hardware.opengl.extraPackages = with pkgs; [
+    vulkan-loader
+    (lib32Packages.mesa)
+    (lib32Packages.vulkan-loader)
+  ];
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 

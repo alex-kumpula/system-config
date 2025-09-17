1795d98dda5045a881da79c252941595dda069d9
Alex Kumpula <alex.kumpula01@gmail.com>
2025-09-17 17:44:42 -0600

    add guest user

--- hosts/alex-laptop/configuration.nix
+++ hosts/alex-laptop/configuration.nix
@@ -275,6 +275,19 @@
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = ["networkmanager" "wheel" "audio" "input" "video"];
     };
+
+    guest = {
+      # TODO: You can set an initial password for your user.
+      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
+      # Be sure to change it (using passwd) after rebooting!
+      initialPassword = "correcthorsebatterystaple";
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+      ];
+      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+      extraGroups = ["networkmanager" "wheel" "audio" "input" "video"];
+    };
   };
 
   # This setups a SSH server. Very important if you're setting up a headless system.

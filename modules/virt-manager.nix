20ea1767b0ace480f37d8a7c7fc30d011f237adf
Alex Kumpula <alex.kumpula01@gmail.com>
2025-09-26 13:21:54 -0600

    enable TMP support for libvirtd

--- modules/virt-manager.nix
+++ modules/virt-manager.nix
@@ -2,4 +2,7 @@
 {
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
+
+  # Enable TPM support for VMs (particularly useful for Windows 11)
+  virtualisation.libvirtd.qemu.swtpm.enable = true;
 }

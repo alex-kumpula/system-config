33240b23c14766af8c9724ef1e24d76117a329bf
Alex Kumpula <alex.kumpula01@gmail.com>
2025-10-17 03:23:20 -0600

    add testIser

--- hosts/alex-laptop/configuration.nix
+++ hosts/alex-laptop/configuration.nix
@@ -301,6 +301,19 @@
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = ["networkmanager" "wheel" "audio" "input" "video"];
     };
+
+    testUser = {
+      # TODO: You can set an initial password for your user.
+      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
+      # Be sure to change it (using passwd) after rebooting!
+      initialPassword = "correcthorsebatterystaple";
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+      ];
+      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+      extraGroups = ["networkmanager" "wheel" "audio" "input" "video" "libvirtd"];
+    };
   };
 
   # This setups a SSH server. Very important if you're setting up a headless system.
